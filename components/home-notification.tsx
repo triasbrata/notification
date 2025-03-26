fe9225aeb5ae0a31eab2e2e015b3439f6708147c
Trias <triasbrata@gmail.com>
2025-03-26 10:33:47 +0700

    bump notification

--- components/home-notification.tsx
+++ components/home-notification.tsx
@@ -3,6 +3,16 @@ export function HomeNotification() {
     <div>
       <h1>Home</h1>
       <a href="/company"> goto company</a>
+      <br />
+      <a href="/notification/home"> goto home</a>
+    </div>
+  );
+}
+export function HomeNotificationTwo() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <a href="/company"> goto company</a>
     </div>
   );
 }
--- index.tsx
+++ index.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HomeNotification } from "./components/home-notification";
+import {
+  HomeNotification,
+  HomeNotificationTwo,
+} from "./components/home-notification";
+const routing = [
+  { path: "/notification", element: <HomeNotification /> },
+  { path: "/notification/home", element: <HomeNotificationTwo />, nav: true },
+];
 export function Hook() {
   // return <h1>Hello</h1>;
-  const router = createBrowserRouter(
-    [{ path: "/", element: <HomeNotification /> }],
-    {
-      basename: "/notification",
-    }
-  );
+  const router = createBrowserRouter(routing);
   return <RouterProvider router={router} />;
 }
